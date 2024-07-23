945002cc8dc0f30de19a5f1f921913f5f5b22010
Sergey Brazhnikov <serjak.2012@gmail.com>
2024-07-23 20:08:30 +0600

    create project, create .editorconfig, delete default unused files, clear app.tsx, install react-bootstrap react-redux @reduxjs-toolkit axios react-toastify

---
+++ src/main.tsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App'
+import {ToastContainer} from "react-toastify";
+import {BrowserRouter} from "react-router-dom";
+// import {Provider} from "react-redux";
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'react-toastify/dist/ReactToastify.min.css';
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+      {/*<Provider store={store}>*/}
+          <BrowserRouter>
+              <ToastContainer position="bottom-left"/>
+              <App/>
+          </BrowserRouter>,
+      {/*</Provider>*/}
+  </React.StrictMode>,
+)
